4d420037595c223c09f5e08aaa62359b1dd2cd30
MorganWaters <morgangeraintwaters@gmail.com>
2018-04-22 09:54:21 +0100

    Work Schedule update
    You can obviously get rid of this if it's just nonsense haha

--- eDepot System/src/eDepotSystem/Schedule.java
+++ eDepot System/src/eDepotSystem/Schedule.java
@@ -3,10 +3,41 @@ package eDepotSystem;
 public class Schedule {
 	
 	static void viewSchedule(String username) {
+		BufferedReader in = new BufferedReader(new FileReader("<work-schedule.txt>"));
+		String line;
+                 while((line = in.readLine()) != null)
+                {
+                 System.out.println(line);
+                }
+                 in.close();
+        	}
+
+        public static void main(String[checkWorkSchedule] args) {
+        Writer writer = null;
+        try {
+            //create a temporary file
+            String timeLog = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
+            File logFile = new File(timeLog);
+
+            // This will output the full path where the file will be written to...
+	    File file = new File ("WorkSchedule/work-schedule.txt")
+		    String path = file.getPath();
+            System.out.println(logFile.getCanonicalPath());
+
+            writer = new Writer(new FileWriter(logFile));
+            writer.write("Booked Time Slot");
 		
-	}
-	
-	static void createSchedule() {
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                // Close the writer regardless of what happens...
+                writer.close();
+            } catch (Exception e) {
+            }
+        }
+    }
+}
 		
 	}
 	
