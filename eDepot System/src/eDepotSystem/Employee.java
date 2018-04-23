31eb4eb338453de64bbb08f31a950c9bff9407be
Emerson McComb <emerson.mc.comb@hotmail.com>
2018-04-23 15:16:52 +0100

    Added view employee, add employee, change employee details, menu for managers

--- eDepot System/src/eDepotSystem/Employee.java
+++ eDepot System/src/eDepotSystem/Employee.java
@@ -1,16 +1,51 @@
 package eDepotSystem;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Employee {
 	
-	static void viewEmployees() {
+	private static final String filePath = "src/eDepotSystem/users.txt";
+	private static Scanner S;
+	private static String username;
+	private static String password;
+	private static String location;
+	private static Boolean isManager;
+	
+	public Employee(String username, String password, String location, Boolean isManager) {
+		this.username = username;
+		this.password = password;
+		this.location = location;
+		this.isManager = isManager;
 		
 	}
 	
-	static void createEmployee() {
-		
+	public static String getUsername() {
+		return username;
 	}
-	
-	static void editEmployee() {
-		
+
+	public static void setUsername(String username) {
+		Employee.username = username;
+	}
+
+	public static void setPassword(String password) {
+		Employee.password = password;
+	}
+
+	public static String getLocation() {
+		return location;
+	}
+
+	public static void setLocation(String location) {
+		Employee.location = location;
+	}
+
+	public static Boolean getIsManager() {
+		return isManager;
+	}
+
+	public static void setIsManager(Boolean isManager) {
+		Employee.isManager = isManager;
 	}
 }
